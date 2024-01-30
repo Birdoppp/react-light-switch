d066d49e6ad06cb6f3d0ec72ee8fa79b34b71620
Bridoppp <tini.pham99@gmail.com>
2024-01-30 13:47:17 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [lightStatus, setLightstatus] = React.useState(false)
+    console.log(lightStatus)
+
     return (
-        <main className="off">
+        <main className={lightStatus === true ? 'on' : "off"}>
             <section>
                 <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <button type="button" onClick={()=> setLightstatus(!lightStatus)}>Turn on/off</button>
+                {lightStatus === false ? 'Turn on' : 'Turn off'}
             </section>
         </main>
     );
